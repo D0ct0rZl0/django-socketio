cdb3327d488d5ac5a4ee2b12527e136c76bb7669
Roman Bagautdinov <doctorzlo1177@gmail.com>
2018-11-01 20:32:32 +0300

    threading and tests_urls

--- django_socketio/management/commands/runserver_socketio.py
+++ django_socketio/management/commands/runserver_socketio.py
@@ -1,6 +1,6 @@
 
 from re import match
-from thread import start_new_thread
+import threading
 from time import sleep
 from os import getpid, kill, environ
 from signal import SIGINT
@@ -50,12 +50,13 @@ class Command(BaseCommand):
         # allowing the port to be set as the client-side default there.
         environ["DJANGO_SOCKETIO_PORT"] = str(self.port)
 
-        start_new_thread(reload_watcher, ())
+        thread = threading.Thread(target=reload_watcher, args=())
+        thread.start()
         try:
             bind = (self.addr, int(self.port))
-            print
-            print "SocketIOServer running on %s:%s" % bind
-            print
+            print()
+            print("SocketIOServer running on %s:%s" % bind)
+            print()
             handler = self.get_handler(*args, **options)
             server = SocketIOServer(bind, handler, resource="socket.io")
             server.serve_forever()
@@ -63,8 +64,8 @@ class Command(BaseCommand):
             client_end_all()
             if RELOAD:
                 server.kill()
-                print
-                print "Reloading..."
+                print()
+                print("Reloading...")
                 restart_with_reloader()
             else:
                 raise
